11fb54f38e0153867e97ca173f112d92bcc21971
Luiz Felipe <gilberto.barbosa@7comm.com.br>
2024-06-10 20:48:08 -0300

    Adiciona teste sobre o retorno de todas as ofertas

---
+++ JornadaMilhas.Test.Integracao/OfertaViagemDalRecuperarTodas.cs
@@ -0,0 +1,27 @@
+﻿using JornadaMilhas.Dados;
+using Xunit.Abstractions;
+
+namespace JornadaMilhas.Test.Integracao;
+
+[Collection(nameof(ContextoCollection))]
+public class OfertaViagemDalRecuperarTodas
+{
+    private readonly JornadaMilhasContext _context;
+
+    public OfertaViagemDalRecuperarTodas(ITestOutputHelper output,
+        ContextoFixture fixture)
+    {
+        _context = fixture.Context;
+        output.WriteLine(_context.GetHashCode().ToString());
+    }
+
+    [Fact]
+    public void ValidaRetornoNaoNulo()
+    {
+        var dal = new OfertaViagemDAL(_context);
+
+        var ofertas = dal.RecuperarTodas();
+
+        Assert.NotNull(ofertas);
+    }
+}
