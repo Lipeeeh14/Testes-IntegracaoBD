8bfabdc791d00d249c54e7328567c577346c812e
Luiz Felipe <gilberto.barbosa@7comm.com.br>
2024-06-10 21:17:12 -0300

    Implementa o docker para subir um container do banco para os testes

--- JornadaMilhas.Test.Integracao/ContextoFixture.cs
+++ JornadaMilhas.Test.Integracao/ContextoFixture.cs
@@ -1,9 +1,10 @@
 ﻿using JornadaMilhas.Dados;
 using Microsoft.EntityFrameworkCore;
+using Testcontainers.MsSql;
 
 namespace JornadaMilhas.Test.Integracao;
 
-public class ContextoFixture
+public class ContextoFixture : IAsyncLifetime
 {
     /*
         Para cada teste é criado uma nova instancia da classe de teste para a sua execução.
@@ -16,12 +17,24 @@ public class ContextoFixture
         Utilizando a implementação do CollectionFixture 
         garante os mesmos recursos para os testes para MAIS DE UMA CLASSE
      */
-    public JornadaMilhasContext Context { get; }
+    public JornadaMilhasContext Context { get; private set; }
+    private readonly MsSqlContainer _msSqlContainer =
+        new MsSqlBuilder()
+        .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
+        .Build();
 
-    public ContextoFixture()
+    public async Task InitializeAsync()
     {
+        await _msSqlContainer.StartAsync();
+
         Context = new JornadaMilhasContext(new DbContextOptionsBuilder<JornadaMilhasContext>()
-            .UseSqlServer("Server=localhost\\SQLEXPRESS;Database=JornadaMilhas;Trusted_Connection=True;MultipleActiveResultSets=true;Encrypt=False")
+            .UseSqlServer(_msSqlContainer.GetConnectionString())
         .Options);
+        Context.Database.Migrate();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _msSqlContainer.StopAsync();
     }
 }
